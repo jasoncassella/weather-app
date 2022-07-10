34cf4385966e84381d28ba88e63a51607a72bc08
Jason Cassella <jasoncassella1@gmail.com>
2022-07-10 18:47:58 -0400

    works if you hit enter button

--- script.js
+++ script.js
@@ -5,10 +5,17 @@ const humidity = document.querySelector('.humidity');
 const wind = document.querySelector('.wind');
 const button = document.querySelector('button');
 const search = document.querySelector('input');
+const icon = document.querySelector('.icon');
 
 function init() {
 	getCurrentLocation();
 }
+
+search.addEventListener('keyup', (e) => {
+	if (e.key === 'Enter') {
+		getLocation();
+	}
+});
 
 button.addEventListener('click', getLocation);
 
@@ -25,7 +32,7 @@ async function getLocation() {
 	if (!search.value) return;
 	const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search.value}&appid=1b79796dec5e64eab2d6444105641fe3`);
 	const data = await response.json();
-	displayData(data)
+	displayData(data);
 }
 
 function success(position) {
@@ -51,6 +58,7 @@ function displayData(data) {
 	description.textContent = data.weather[0].description;
 	humidity.textContent = `Humidity: ${data.main.humidity}%`;
 	wind.textContent = `Wind Speed: ${data.wind.speed} mph`;
+	icon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
 }
 
 function kToF(kelvin) {
